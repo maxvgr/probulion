c475549fbb7051f4d43a09a6e72582ad427fc280
Max V Grushin <support@ids.spb.ru>
2024-08-22 15:32:33 +0300

    feat: добавлен fancybox

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -1,8 +1,10 @@
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
 const hamburger = document.querySelector(".hamburger--spin");
 const mobileNavs = document.querySelector(".header__menu-mobile");
 const catalog = document.querySelector(".mobile-catalog");
 const hiddenmenu = document.querySelector(".mobile-catalog__hidden-menu");
-
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");
@@ -14,4 +16,8 @@ catalog.addEventListener("click", () => {
   hiddenmenu.classList.toggle("is-active");
   catalog.classList.toggle("is-active");
   // overlay.classList.toggle("is-active");
+});
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
 });
