c2659a59b26fb0ead2db770dc6358a83e432635f
Max V Grushin <support@ids.spb.ru>
2024-09-04 13:36:54 +0300

    fix: правки по скриптам

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -6,12 +6,13 @@ const mobileNavs = document.querySelector(".header__menu-mobile");
 const catalog = document.querySelector(".mobile-catalog");
 const hiddenmenu = document.querySelector(".mobile-catalog__hidden-menu");
 const popup = document.querySelector(".footer-mail__popup");
+const popupmailopen = document.querySelector(".mail-popup__open");
 const popupclose = document.querySelector(".footer-mail__close");
 const popupcard = document.querySelector(".footer-card__popup");
 const popupcardclose = document.querySelector(".footer-card__close");
 const popupcardclosebtn = document.querySelector(".footer-card__close--button");
 const popupcardopen = document.querySelector(".card-popup__open");
-const popupmailopen = document.querySelector(".mail-popup__open");
+
 
 
 hamburger.addEventListener("click", () => {
@@ -26,30 +27,34 @@ catalog.addEventListener("click", () => {
   // overlay.classList.toggle("is-active");
 });
 
-// popupclose.addEventListener("click", () => {
-//   popup.classList.add("hidden");
-// });
 
-popupmailopen.addEventListener("click", () => {
-  popup.classList.toggle("is-active");
-});
+if (popupmailopen.getAttribute('listener') !== 'true') {
+  popupmailopen.addEventListener("click", () => {
+    popup.classList.toggle("is-active");
+  });
+}
 
-popupclose.addEventListener("click", () => {
-  popup.classList.toggle("is-active");
-});
+if (popupclose.getAttribute('listener') !== 'true') {
+  popupclose.addEventListener("click", () => {
+    popup.classList.toggle("is-active");
+  });
+}
 
-popupcardopen.addEventListener("click", () => {
-  popupcard.classList.toggle("is-active");
-});
 
-popupcardclose.addEventListener("click", () => {
-  popupcard.classList.toggle("is-active");
-});
+if (document.querySelector('#catalog-inter')) {
+  popupcardopen.addEventListener("click", () => {
+    popupcard.classList.toggle("is-active");
+  });
 
-popupcardclosebtn.addEventListener("click", () => {
-  popupcard.classList.toggle("is-active");
-});
+  popupcardclose.addEventListener("click", () => {
+    popupcard.classList.toggle("is-active");
+  });
 
-Fancybox.bind("[data-fancybox]", {
-  // Your custom options
-});
+  popupcardclosebtn.addEventListener("click", () => {
+    popupcard.classList.toggle("is-active");
+  });
+
+  Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
+}
